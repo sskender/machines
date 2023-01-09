a833222185ab71e800640e767dacd765f22e02cd
Sven Skender <sskender@pm.me>
2023-01-09 12:16:43 +0100

    Move task duration to config

--- flask-celery/config.py
+++ flask-celery/config.py
@@ -2,5 +2,7 @@ import os
 
 
 class Config:
+    TASK_DURATION = 10
+
     CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", "redis://localhost:6379/0")
     RESULT_BACKEND = os.getenv("RESULT_BACKEND", "redis://localhost:6379/0")
--- flask-celery/tasks.py
+++ flask-celery/tasks.py
@@ -1,12 +1,13 @@
 import time
 from app import celery
 from flask import current_app
+from config import Config
 
 
 @celery.task(name="heavy_math_task")
 def heavy_math_task(n):
     app = current_app._get_current_object()
     app.logger.info(f"New task received: n = {n}")
-    time.sleep(5)
+    time.sleep(Config.TASK_DURATION)
     r = n**2
     app.logger.info(f"Task finished: n = {n} r = {r}")
